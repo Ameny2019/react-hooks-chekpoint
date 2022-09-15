0318fdddd7d98227c067aa8a3c1441ab778c0af4
ameny <ouelhazi.amani19@gmail.com>
2022-09-15 19:43:35 +0100

    first commit

---
+++ src/components/MoviesData.js
@@ -0,0 +1,51 @@
+export const moviesData = [
+  {
+    image: "https://m.media-amazon.com/images/I/51TKV2HF1TL._SY445_.jpg",
+    rating: 5,
+    name: "A walk to remember",
+    date: "2002",
+    description:
+      "The film, as with the book, is dedicated to Sparks' sister Danielle, whose cancer-afflicted life inspired the story. ,Romance Drama",
+  },
+  {
+    image: "https://m.media-amazon.com/images/M/MV5BZjJiYTExOTAtNWU0Yi00NzJjLTkwOTgtOTU2NWM1ZjJmYWVhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    rating: 4,
+    name: "Maléfique: Le Pouvoir du Mal",
+    date: "2018",
+    description:
+    "La relation complexe entre Maléfique et Aurore alors qu'elles font face à de nouvelles menaces pour leur pays magique."
+  },
+  {
+    image: "https://image.tmdb.org/t/p/w500/pjeMs3yqRmFL3giJy4PMXWZTTPa.jpg",
+    rating: 4,
+    name: "Frozen II",
+    date: "2019",
+    description:
+      "Anna, Elsa, Kristoff, Olaf and Sven leave Arendelle to travel to an ancient, autumn-bound forest of an enchanted land.",
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51oDs32SXlL._AC_.jpg",
+    rating: 3,
+    name: "Fantastic Beasts and Where to Find Them",
+    date: "2016",
+    description:
+      "The adventures of writer Newt Scamander in New York's secret community of wizards",
+  },
+  {
+    image:
+      "https://i.ibb.co/FDGqCmM/papers-co-ag74-interstellar-wide-space-film-movie-art-33-iphone6-wallpaper.jpg",
+    rating: 5,
+    name: "Interstellar",
+    date: "2014",
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+  {
+    image:"https://m.media-amazon.com/images/M/MV5BZmRjODgyMzEtMzIxYS00OWY2LTk4YjUtMGMzZjMzMTZiN2Q0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    rating: 5,
+    name: "Red Notice",
+    date: "2021",
+    description:"An Interpol agent successfully tracks down the world's most wanted art thief, with help from a rival thief. But nothing is as it seems, as a series of double-crosses ensue."
+  },
+];
